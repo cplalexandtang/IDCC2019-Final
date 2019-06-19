d04efddff33ea9ddb1c821cf901030111812f8d5
photon00 <b04901003@ntu.edu.tw>
2019-06-19 19:44:53 +0800

    use jquery

--- websocket/public/client.js
+++ websocket/public/client.js
@@ -1,41 +1,44 @@
+(function ($) {
+  
+  var socket = new WebSocket('ws://localhost:8081/');
+  socket.onopen = (event) => {
+    log('Opened connection 🎉');
+    var json = JSON.stringify({ message: 'Hello' });
+    socket.send(json);
+    log('Sent: ' + json);
+  }
 
-var socket = new WebSocket('ws://localhost:8081/');
-socket.onopen = (event) => {
-  log('Opened connection 🎉');
-  var json = JSON.stringify({ message: 'Hello' });
-  socket.send(json);
-  log('Sent: ' + json);
-}
+  socket.onerror = (event) => {
+    log('Error: ' + JSON.stringify(event));
+  }
 
-socket.onerror = function(event) {
-  log('Error: ' + JSON.stringify(event));
-}
+  socket.onmessage = (event) => {
+    log('Received: ' + event.data);
+  }
 
-socket.onmessage = function (event) {
-  log('Received: ' + event.data);
-}
+  socket.onclose = (event) => {
+    log('Closed connection 😱');
+  }
 
-socket.onclose = function(event) {
-  log('Closed connection 😱');
-}
+  $('#close').on('click', (event) => {
+    socket.close();
+    log('Closed connection 😱');
+  });
 
-document.querySelector('#close').addEventListener('click', function(event) {
-  socket.close();
-  log('Closed connection 😱');
-});
+  $('#send').on('click', (event) => {
+    var json = JSON.stringify({ message: 'Hey there' });
+    socket.send(json);
+    log('Sent: ' + json);
+  });
 
-document.querySelector('#send').addEventListener('click', function(event) {
-  var json = JSON.stringify({ message: 'Hey there' });
-  socket.send(json);
-  log('Sent: ' + json);
-});
+  var log = (text) => {
+    var li = document.createElement('li');
+    li.innerHTML = text;
+    $('#log').append(li);
+  }
 
-var log = (text) => {
-  var li = document.createElement('li');
-  li.innerHTML = text;
-  document.getElementById('log').appendChild(li);
-}
+  window.addEventListener('beforeunload', function() {
+    socket.close();
+  });
 
-window.addEventListener('beforeunload', function() {
-  socket.close();
-});
+})(jQuery);
